f5dc80f7be4a878d283315adea98a6e4baacbd60
Abilash-Konganar <88280554+Abilash-K@users.noreply.github.com>
2023-11-14 21:49:12 +0530

    Create Sell_Stocks.py

---
+++ Sell_Stocks.py
@@ -0,0 +1,18 @@
+def stocks(prices):
+  l,r=0,1
+  max_profit = 0
+  
+  while r<len(prices):
+    if prices[l]<prices[r]:
+      profit=prices[r]-prices[l]
+      max_profit=max(max_profit,profit)
+    else:
+      l=r
+    r+=1
+  return max_profit
+
+
+
+prices = [7,1,5,3,6,4]
+result = stocks(prices)
+print(result)
